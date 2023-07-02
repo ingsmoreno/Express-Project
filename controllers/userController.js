03bca079e0c8dc1f36791d3fff61ffc46d4cb789
saray-moreno <saray.moreno@globant.com>
2023-07-02 00:23:08 -0300

    setting up user configuration and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'method not defined'
+    })
+} 
+
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'method not defined'
+    })
+} 
+
+exports.getUserById = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'method not defined'
+    })
+} 
+
+exports.updateUserById = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'method not defined'
+    })
+} 
+
+exports.deleteUserById = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'method not defined'
+    })
+} 
