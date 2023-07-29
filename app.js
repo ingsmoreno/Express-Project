5abff6b8a9f71bf3c8527089b03362a04da51f14
saray-moreno <saray.moreno@globant.com>
2023-07-29 12:34:12 -0300

    middleware configuration

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { initializeAPI } = require('./initializeAPI');
+const dotenv = require('dotenv')
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
@@ -7,6 +7,8 @@ const userRouter = require('./routes/userRoutes')
 
 
 const app = express();
+
+dotenv.config({path: './config.env'});
 
 console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'development'){
@@ -19,10 +21,6 @@ app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
-
-
-app.get('/', initializeAPI)
-
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter)
