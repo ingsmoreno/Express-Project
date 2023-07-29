8bfcf97c75635af6e058ab207a0faad3cd501e73
saray-moreno <saray.moreno@globant.com>
2023-07-29 12:26:12 -0300

    connecting mongoose to server

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
-dotenv.config({path: './config.env'});
 const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', process.env.DATABASE_PASSWORD
+    );
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(() => console.log('Succesfully connection to MongoDB'))
+.catch(err => {
+    if(err) throw Error("Connect error to MongoDB")}
+    )
+
+
+// const TourDoc = new Tour({
+//     name: 'The Park Camper',
+//     price: 297,
+// })
+
+// TourDoc.save().then(doc => {
+//     console.log(doc)
+// }).catch(err => console.log(err))
 
 const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
