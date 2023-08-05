fb34bad085bb8ec397426321917b00a8c45f3246
saray-moreno <saray.moreno@globant.com>
2023-08-05 12:37:08 -0300

    adding getMonthlyPlan route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const {getAllTours, postTour, getTourById, patchTour,deleteTour, aliasTopTours, getTourStats} = require('./../controllers/tourController')
+const { getAllTours, postTour, getTourById, patchTour,deleteTour, aliasTopTours, getTourStats, getMonthlyPlan } = require('./../controllers/tourController')
 
 const router = express.Router();
 
 // router.param('id', checkId);
 
 router
-.route('/tour-stats')
-.get(getTourStats);
+    .route('/tour-stats')
+    .get(getTourStats);
 
 router
-.route('/top-5-cheap')
-.get(aliasTopTours, getAllTours);
+    .route('/monthly-plan/:year')
+    .get(getMonthlyPlan);
+
+router
+    .route('/top-5-cheap')
+    .get(aliasTopTours, getAllTours);
 
 router
     .route('/')
