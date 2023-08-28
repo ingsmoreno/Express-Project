b17121f094901a611fb15a43856949138289e01a
saray-moreno <saray.moreno@globant.com>
2023-08-28 17:56:40 -0300

    adding the singup method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.singup = catchAsync(async (req, res) => {
+    const newUser = await User.create(req.body); 
+
+    res.status(201).json({
+        status: 'sucess',
+        data: {
+            user: newUser
+        }
+    })
+})
+
 exports.getAllUsers = (req, res) => {
     res.status(500).json({
         status: 'error',
