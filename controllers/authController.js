1128892abb36bac7b802c93bdf96150908831677
saray-moreno <saray.moreno@globant.com>
2023-08-29 17:22:31 -0300

    adding role property

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,8 @@ exports.singup = catchAsync(async (req, res) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
-        passwordChangedAt: req.body.passwordChangedAt
+        passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role
     }); 
 
     const token = signToken(newUser._id);
@@ -72,3 +73,15 @@ exports.protect = catchAsync (async (req, res, next) => {
     req.user = currentUser;
     next();
 })
+
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            console.log('does not include')
+            return next(new AppError('Permission denied', 403));
+        }
+
+        next();
+    }
+}
