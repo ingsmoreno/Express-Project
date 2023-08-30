bede92c3891815fb25330d7da17c79efa695c002
saray-moreno <saray.moreno@globant.com>
2023-08-30 13:27:37 -0300

    setting up createPasswordResetToken using crypto

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -33,16 +34,25 @@ const userSchema = new mongoose.Schema({
         },
         select: false
     }, 
-    passwordChangedAt: Date,
     role: {
         type: String, 
         enum: ['admin', 'guide', 'user', 'lead-guide'],
         default: 'user'
-    }
+    }, 
+    passwordChangedAt: Date,
+    passwordResetToken: String, 
+    passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function(next){
+
+    //Only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    //Hash the password with cost of 12
     this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete passwordConfirm field
     this.passwordConfirm = undefined;
     next()
 })
@@ -59,6 +69,22 @@ userSchema.methods.changedPasswordAfter = function(JWTTimeStamp){
 
     return false;
    
+}
+
+userSchema.methods.createPasswordResetToken = function(){
+    const resetToken = crypto
+        .randomBytes(32)
+        .toString('hex');
+    
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+
 }
 
 const User = mongoose.model('User', userSchema);
