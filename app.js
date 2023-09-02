9f832ff0c3bff1f42af714db80a01b1e3abe2bc4
saray-moreno <saray.moreno@globant.com>
2023-09-02 16:22:25 -0300

    setting up the mongo sanitize, xss and hpp

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { rateLimit } = require('express-rate-limit');
 const dotenv = require('dotenv')
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -6,20 +7,56 @@ const userRouter = require('./routes/userRoutes');
 const authRouter = require('./routes/authRoutes');
 const AppError = require('./utils/appError');
 const globalHandlerError = require('./controllers/errorController')
-
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp')
 
 const app = express();
 
+//Set security HTTP headers
+app.use(helmet());
+
 dotenv.config({path: './config.env'});
+
+const rateLimiting = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again after an hour',
+})
+// Data Sanitization agains NoSQL query injection
+
+
+app.use('/api', rateLimiting);
 
 console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
-app.use(express.json());
 
+//Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+//ALL THESE CONFIGURATIONS SHOULD BE AFTER CALLING BODY PARSER
+app.use(mongoSanitize());
+
+//Data Sanitization agains XSS
+app.use(xss());
+
+//HTTP Parameter Pollution
+app.use(hpp({
+    whitelist: 
+    ['duration',
+    'ratingAverage', 
+    'ratingQuantity', 
+    'difficulty', 
+    'price']
+}))
+
+
+
+//Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
    // console.log(x);
