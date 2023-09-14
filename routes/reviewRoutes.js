d36ae1917969138294ce3d683266cc2fc3aa12a6
saray-moreno <saray.moreno@globant.com>
2023-09-14 09:10:30 -0300

    updating review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllReviews, createReview, deleteReviewById } = require('../controllers/reviewController');
+const { getAllReviews, createReview, deleteReviewById, updateReviewById, setTourAndUser } = require('../controllers/reviewController');
 const {protect, restrictTo} = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
@@ -8,10 +8,11 @@ router.route('/')
     .get(getAllReviews)
     .post(
         protect, 
-        restrictTo('user'), 
+        restrictTo('user'), setTourAndUser,
         createReview)
 
 router.route('/:id')
+    .patch(protect, restrictTo('user'),  updateReviewById)
     .delete(
         protect, 
         restrictTo('admin'), 
