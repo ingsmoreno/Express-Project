3c5da5a5c8bdbfd9172e78d3cc80bba2fc3e8d21
saray-moreno <saray.moreno@globant.com>
2023-09-16 12:52:06 -0300

    adding updateMe and deleteMe

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,37 @@ exports.getMe = (req, res, next) => {
     next();
 }
 
+exports.updateMe = catchAsync( async (req, res, next) => {
+    // 1) Create error if user post password data
+    if(req.body.password || req.body.passwordConfirm ) return next(new AppError('The password cannot be updated. Please update password in /updatePassword', 401));
+
+    // 2) Filter put wanted fields that are allowed to be updated
+    const filterFields = filterRoles(req.body, 'name', 'email');
+    const updateUser = await User.findByIdAndUpdate({_id: req.user._id}, filterFields, {
+        new: true,
+        runValidators: true
+    })
+
+    // 3) Update user document
+
+    res.status(201).json({
+        status: 'sucess',
+        data: {
+            updateUser
+        }
+    })
+})
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndUpdate({_id: req.user._id}, {active: false});
+    
+    res.status(204).json({
+        status: 'sucess',
+        data: null
+    })
+
+})
+
 exports.getAllUsers = getAll(User);
 exports.getOneUser = getOne(User);
 exports.updateUser = updateOne(User); // REVIEW AUTHORIZATION, PASSWORD CANNOT BE UPDATED HERE! 
