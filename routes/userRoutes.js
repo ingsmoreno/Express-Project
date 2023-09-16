82bcbbb0423dbb64866613f464f027605348ed3e
saray-moreno <saray.moreno@globant.com>
2023-09-16 12:52:53 -0300

    adding middleware to protect and restrictTo and routes to delete me update me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const { getAllUsers, getMe, getOneUser, updateUser, deleteUserById} = require ('./../controllers/userController');
-const { protect, restrictTo } = require('./../controllers/authController')
+const { getAllUsers, getMe, getOneUser, updateUser, deleteUserById, updateMe, deleteMe } = require ('./../controllers/userController');
+const { protect, restrictTo} = require('./../controllers/authController')
 
 const router = express.Router();
+
+router.use(protect);
 
 router.route('/')
     .get(getAllUsers);
 
 router.route('/me')
-    .get(protect, getMe, getOneUser);
+    .get(getMe, getOneUser);
+
+router.route('/updateMe')   
+    .patch(updateMe);
+
+router.route('/deleteMe')
+    .delete(deleteMe);
+
+router.use(restrictTo('admin'));
 
 router.route('/:id')
     .get(getOneUser)
     .patch(updateUser)
-    .delete(protect, restrictTo('admin'), deleteUserById);
+    .delete(deleteUserById);
 
 module.exports = router;
