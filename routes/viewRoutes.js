48e5786201081bd3bb5e7758c293c0c9018873c2
saray-moreno <saray.moreno@globant.com>
2023-10-21 14:49:43 -0300

    setting up nthe routes for submit data user

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const { getOVerview, getTours, getLogin, getMe} = require('./../controllers/viewController');
+const { getOVerview, getTours, getLogin, getMe, updatingUserData} = require('./../controllers/viewController');
 const { loggedIn, protect } = require('../controllers/authController');
 
 const router = express();
 
 router.get('/', loggedIn, getOVerview );
 
-router.get('/tour/:slug', loggedIn, getTours)
+router.get('/tour/:slug', loggedIn, getTours);
 
-router.get('/login',loggedIn, getLogin)
+router.get('/login', loggedIn, getLogin);
 
-router.get('/me', protect, getMe)
+router.get('/me', protect, getMe);
+
+router.post('/submit-user-data',protect, updatingUserData );
 
 module.exports = router;
